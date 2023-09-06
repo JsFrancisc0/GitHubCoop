d9edcd1de28f7650cbc74f399e02affa67461b95
JsFrancisc0 <j.saldias03@ufromail.cl>
2023-09-06 01:15:03 -0300

    holi

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,5 +3,6 @@ public class HolaMundo {
     public static void main(String[] args){
 
         System.out.println("Hola Mundo!!!!");
+        System.out.println("Adios Mundo... - J Francisco Saldias 06/09/23");
     }
 }
